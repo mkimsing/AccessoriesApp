b107b885b4cca23c949cebd2474270fef0caee74
LAPTOP-0PN2T9GL\MKS <mkimsing95@gmail.com>
2019-07-03 17:50:06 -0700

    Working on NavBar

---
+++ client/src/components/navBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function navBar() {
+  return (
+    <div className="navBar_content">
+      <section className="header">
+        <div className="instagram_icon"> </div>
+        <div className="pintrest_icon"> </div>
+        <h3> Broadcast </h3>
+        <div className="search_icon"> </div>
+        <div className="cart_icon"> </div>
+      </section>
+    </div>
+  );
+}
