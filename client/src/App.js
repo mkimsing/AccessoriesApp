1fe36d8977111c40d93629e7cf5a370bc9e5e955
Rosa Kang <therosessom@gmail.com>
2019-07-06 15:23:48 -0700

    after styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./styles/App.css";
+import Collection from "../src/components/Collection";
+import Categories from "../src/components/Categories";
 
 function App() {
   return (
     <div className="App">
       <h1> Placeholder Text </h1>
+      <Categories />
+      <Collection />
     </div>
   );
 }
