67992ce26e6bc7b047f4c7dc625ce851544eb8cf
Rosa Kang <therosessom@gmail.com>
2019-07-09 13:32:50 -0700

    common product card

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/App.css";
 import Collection from "../src/components/Collection";
 import Categories from "../src/components/Categories";
+import Common from "./components/Common";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1> Placeholder Text </h1>
       <Categories />
       <Collection />
+      <Common />
     </div>
   );
 }
